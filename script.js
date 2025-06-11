54eab0d8559141aa4043be59fe81db8425c32ebf
QuietOne21 <167322839+QuietOne21@users.noreply.github.com>
2025-06-11 06:51:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-<script>
-  document.addEventListener("DOMContentLoaded",function(){
+ document.addEventListener("DOMContentLoaded",function(){
     const form=document.querySelector("form");
     form.addEventListner("submit",function(e){
       e.preventDefault();
       window.location.href="index2.html";
     });
   });
-    
-  </script>
