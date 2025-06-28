00ae5ac1440e53251597f1a691116739590a3cd2
QuietOne21 <167322839+QuietOne21@users.noreply.github.com>
2025-06-28 17:55:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,8 @@
       window.location.href="index2.html";
   });
 
-const form=document.getElementById(
+const form=document.getElementById("signUpForm");
+form.addEventListener("signIn",function(e){
+  e.preventDefault();
+  window.location.href="index1.html";
+});
