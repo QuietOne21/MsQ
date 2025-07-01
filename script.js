a4b06fcae83cac88ec3b367d24213d397977e5c2
QuietOne21 <167322839+QuietOne21@users.noreply.github.com>
2025-07-01 21:16:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,15 +11,7 @@ form.addEventListener("signIn",function(e){
 });
 
 
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyD0V9jyO2m6ZjXmJxDrKaOyCTonvtpKfKU",
     authDomain: "reneq-25db.firebaseapp.com",
@@ -30,7 +22,8 @@ form.addEventListener("signIn",function(e){
     measurementId: "G-CSRJ8Y1F2X"
   };
 
-  // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-</script>
+
+
+
