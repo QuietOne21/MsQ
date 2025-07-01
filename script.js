c3d7b44e10223fed46b04def1e8f2bf3a17d2afc
QuietOne21 <167322839+QuietOne21@users.noreply.github.com>
2025-07-01 21:58:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyD0V9jyO2m6ZjXmJxDrKaOyCTonvtpKfKU",
+    authDomain: "reneq-25db.firebaseapp.com",
+    projectId: "reneq-25db",
+    storageBucket: "reneq-25db.firebasestorage.app",
+    messagingSenderId: "87519655030",
+    appId: "1:87519655030:web:b6874d99338c4c199f1b57",
+    measurementId: "G-CSRJ8Y1F2X"
+  };
+ 
+  
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+
 //Handle Login Form
 const loginForm = document.getElementById("loginForm");
  if (loginForm){
@@ -29,29 +44,5 @@ if(signUpForm){
   window.location.href = "index1.html";
  });
 }
-
-
-
-
-
-
-
-
-
- 
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyD0V9jyO2m6ZjXmJxDrKaOyCTonvtpKfKU",
-    authDomain: "reneq-25db.firebaseapp.com",
-    projectId: "reneq-25db",
-    storageBucket: "reneq-25db.firebasestorage.app",
-    messagingSenderId: "87519655030",
-    appId: "1:87519655030:web:b6874d99338c4c199f1b57",
-    measurementId: "G-CSRJ8Y1F2X"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-
 
 
